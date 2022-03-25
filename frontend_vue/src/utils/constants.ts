09600ef8bc833d68ff8f999ea19fd191932b4016
Archie <411461424@qq.com>
2022-03-25 22:55:12 +0800

    删除一些敏感文件

--- frontend_vue/src/utils/constants.ts
+++ frontend_vue/src/utils/constants.ts
@@ -1,7 +1,6 @@
 class Constants {
    apiUrl = 'http://localhost:8000/api/'
-    // apiUrl = 'http://106.14.191.138/api/'
-    //  apiUrl = 'http://www.minzufs.cn/api/'
+
 }
 
 const constant = new Constants()
